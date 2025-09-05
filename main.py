84bca5b0c8187abdfe400016485e14182e7e91ec
Josh Birch <joshuabirch@protonmail.com>
2025-09-05 11:35:01 +0100

    Add functionality to pass in prompt via command line argument and error handling for no input

--- main.py
+++ main.py
@@ -8,13 +8,18 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
-    print("Hello from ai-agent!")
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    response_tokens = response.usage_metadata.candidates_token_count
-    print(response.text)
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: {response_tokens}")
+    print("Hello from ai-agent!\n")
+    
+    if len(sys.argv) < 2:
+        print("Please enter a prompt.")
+        sys.exit(1)
+    else:
+        response = client.models.generate_content(model="gemini-2.0-flash-001", contents=sys.argv[1])
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
+        print(response.text)
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
 
 if __name__ == "__main__":
     main()
