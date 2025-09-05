1cfb3ee172095011039585346f27615146f09e78
Josh Birch <joshuabirch@protonmail.com>
2025-09-05 12:24:23 +0100

    add --verbose flag to CLI for debug output

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -14,12 +15,21 @@ def main():
         print("Please enter a prompt.")
         sys.exit(1)
     else:
-        response = client.models.generate_content(model="gemini-2.0-flash-001", contents=sys.argv[1])
+        verbose = "--verbose" in sys.argv
+        user_prompt = sys.argv[1]
+        messages = [
+                types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        ] 
+        response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
         prompt_tokens = response.usage_metadata.prompt_token_count
         response_tokens = response.usage_metadata.candidates_token_count
+        
+        if verbose:
+            print(f"User prompt: {user_prompt}")
+            print(f"Prompt tokens: {prompt_tokens}")
+            print(f"Response tokens: {response_tokens}")
+
         print(response.text)
-        print(f"Prompt tokens: {prompt_tokens}")
-        print(f"Response tokens: {response_tokens}")
 
 if __name__ == "__main__":
     main()
