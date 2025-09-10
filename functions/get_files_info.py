1b24e007f469a099f5d6440b1b0be93fd565c63b
Josh Birch <joshuabirch@protonmail.com>
2025-09-10 13:05:58 +0100

    Add schema for get_files_info function and integrate into main application

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,9 +1,25 @@
 import os
+from google.genai import types
+
+schema_get_files_info = types.FunctionDeclaration(
+            name = "get_files_info",
+            description = "Lists files in the specified directory along with their sizes, constrained to the working directory.",
+            parameters = types.Schema(
+                type = types.Type.OBJECT,
+                properties = {
+                    "directory": types.Schema(
+                        type = types.Type.STRING,
+                        description = "The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself",
+                        ),
+                    },
+                ),
+            )
 
 def get_files_info(working_directory, directory="."):
     abs_target = os.path.abspath(os.path.join(working_directory, directory))
     abs_work = os.path.abspath(working_directory)
     prefix = abs_work + os.path.sep
+    
 
     if not (abs_target == abs_work or abs_target.startswith(prefix)):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
